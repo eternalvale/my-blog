0fd2487151fdf184a848f71571cab42dcaa81f3d
Anthony <anthonyvsamaroo@gmail.com>
2020-02-21 12:56:14 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Anthony Samaroo`,
+    author: `Anthony Samaroo`,
+    description: `Professional writer and content creator.`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    Professional writer and content creator.
+    <br />
+    Specialities include; technology, health, and coffee.
+  </Fragment>
+)
